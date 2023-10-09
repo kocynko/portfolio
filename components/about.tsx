5011f1f91fcd675890f5437250084b1f533e3158
Matus Kocian <matus.kociann@gmail.com>
2023-10-09 23:43:17 +0200

    feat: intro

---
+++ components/about.tsx
@@ -0,0 +1,43 @@
+"use client"
+import React from "react"
+import SectionHeading from "./section-heading"
+import { motion } from "framer-motion"
+export default function About() {
+  return (
+    <motion.section
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      className="mb-28 max-w-[45rem] text-center leading-8"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        In the pursuit of{" "}
+        <span className="font-medium">Applied Informatics</span> at the Slovak
+        University of Technology, I have been deeply engaged in the field of{" "}
+        <span className="underline">Machine Learning</span> and{" "}
+        <span className="underline">Deep Neural Networks</span>. Additionally, I
+        found that I'm best in frontend development and enjoy it the most.{" "}
+        <span className="italic">
+          Problem-solving in programming is a strong suit of mine.
+        </span>{" "}
+        My skill set encompasses{" "}
+        <span className="font-medium">React, Next.js, Node.js, MySQL</span>, and{" "}
+        <span className="font-medium">Python</span> with frameworks such as{" "}
+        <span className="font-medium">Tensorflow</span>. At present, I am
+        immersed in the study of <span className="underline">TypeScript</span>,
+        and the journey thus far has been highly rewarding. I am actively
+        seeking a full-time position as a software developer.
+      </p>
+
+      <p>
+        <span className="italic">Beyond the realm of coding</span>, I am
+        committed to maintaining an active presence in the gym, derive pleasure
+        from video gaming, and appreciate the art of cinema. I value the
+        creation of enduring memories with cherished companions. They say I'm a{" "}
+        <span className="font-medium">quick learner</span>, always eager to
+        absorb new skills, especially in areas that ignite my passion.
+      </p>
+    </motion.section>
+  )
+}
---
+++ components/intro.tsx
@@ -0,0 +1,95 @@
+"use client"
+import Image from "next/image"
+import React from "react"
+import profilePic from "@/public/profile.jpg"
+import { motion } from "framer-motion"
+import Link from "next/link"
+import { BsArrowRight, BsLinkedin } from "react-icons/bs"
+import { HiDownload } from "react-icons/hi"
+import { FaGithubSquare } from "react-icons/fa"
+
+export default function Intro() {
+  return (
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: "tween", duration: 0.2 }}
+          >
+            <Image
+              src={profilePic}
+              alt="Matus portrait"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.5,
+              duration: 0.7,
+            }}
+            className="absolute bottom-0 right-0 text-4xl"
+          >
+            🙌
+          </motion.span>
+        </div>
+      </div>
+      <motion.h1
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        className="sm:text-4l mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5]"
+      >
+        <span className="font-bold">Hello, I'm Matus.</span> I'm a{" "}
+        <span className="font-bold">student of applied informatics</span> with
+        desire to be <span className="font-bold">frontend developer</span>.I
+        enjoy building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>.
+      </motion.h1>
+      <motion.div
+        className="flex flex-col items-center justify-center gap-2 px-4 text-lg font-medium sm:flex-row"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+      >
+        <Link
+          href="#contact"
+          className="group flex items-center gap-2 rounded-full bg-gray-900 px-7 py-3 text-white outline-none transition hover:scale-110 hover:bg-gray-950 focus:scale-110 active:scale-105 "
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 transition group-hover:translate-x-1.5" />
+        </Link>
+        <a
+          className="group flex cursor-pointer items-center gap-2 rounded-full border border-black/10 bg-white px-7 py-3 outline-none transition hover:scale-110 focus:scale-110 active:scale-105"
+          href="/CV.pdf"
+          download
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 transition group-hover:translate-y-1" />
+        </a>
+        <a
+          className="border-black/10active:scale-105 flex cursor-pointer items-center rounded-full border bg-white p-4 text-gray-700 transition hover:scale-[1.15]  hover:text-gray-950 focus:scale-[1.15]"
+          href="https://www.linkedin.com/in/matuskocian/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          className="flex cursor-pointer items-center rounded-full border border-black/10 bg-white p-4 text-gray-700 transition hover:scale-[1.15] hover:text-gray-950 focus:scale-[1.15] active:scale-105"
+          href="https://github.com/kocynko"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
+    </section>
+  )
+}
