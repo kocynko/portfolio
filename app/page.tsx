a0fe4485c9f7dc665c8b3e2911a52d3848b98257
Matus Kocian <matus.kociann@gmail.com>
2023-10-18 10:12:05 +0200

    place footer in right file

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import About from "@/components/about";
 import Experience from "@/components/experience";
-import Footer from "@/components/footer";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
@@ -15,7 +14,6 @@ export default function Home() {
       <Projects />
       <Skills />
       <Experience />
-      <Footer />
     </main>
   );
 }
